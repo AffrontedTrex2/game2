ef1ba689ac3f40168501680b1f6a506a8acb0a2d
Jasmine Zhang <jasminezhang@live.com>
2017-09-07 19:53:27 -0700

    game

---
+++ Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class DeathTrigger : MonoBehaviour {
+
+    public Health health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("yes");
+            health.ChangeHealth(-1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
